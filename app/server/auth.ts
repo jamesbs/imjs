14aea6464b42df06d5f3259ceb51681e76e4d9a4
jamesbs <osdev@jamesbs.com>
2024-01-30 22:56:56 -0500

    switch from elysia to express

---
+++ app/server/auth.ts
@@ -0,0 +1,53 @@
+import { Express, RequestHandler } from 'express'
+import expressSession from 'express-session'
+import passport from 'passport'
+import { Strategy as GithubStrategy } from 'passport-github2'
+import { VerifyCallback } from 'passport-oauth2'
+
+export type GithubUser = {
+  id: number
+}
+
+export const githubStrategy = new GithubStrategy(
+  {
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: `${process.env.BASE_URL}/auth/github/callback`,
+  },
+  (accessToken: string, _: string, user: GithubUser, done: VerifyCallback) => {
+    return done(null, user)
+  }
+)
+
+export const setupAuth = (app: Express) => {
+  app.use(passport.initialize())
+
+  passport.serializeUser((user: any, done) => {
+    done(null, user.id)
+  })
+
+  passport.deserializeUser((user: any, done) => {
+    done(null, user)
+  })
+
+  passport.use(githubStrategy)
+
+  app.use(
+    expressSession({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.ENVIRONMENT === 'production',
+      },
+    })
+  )
+  app.use(passport.session())
+}
+
+export const authenticationRequired: RequestHandler = (
+  request,
+  response,
+  next
+) => (request.isAuthenticated() ? next() : response.redirect('/'))
---
+++ app/server/express.ts
@@ -0,0 +1,42 @@
+import express from 'express'
+import passport from 'passport'
+import { resolve } from 'path'
+import { paths } from '../../env.ts'
+import { authenticationRequired, setupAuth } from './auth.ts'
+
+export const serve = async ({ port = 3000 }) => {
+  const app = express()
+
+  app.use(express.static(paths.dist))
+  setupAuth(app)
+
+  app
+    .get(
+      '/auth/github',
+      passport.authenticate('github', { scope: ['user:email'] })
+    )
+    .get(
+      '/auth/github/callback',
+      passport.authenticate('github', { failureRedirect: '/login' }),
+      (...[, response]) => {
+        response.redirect('/auth-gated')
+      }
+    )
+    .get('/auth-gated', authenticationRequired, (request, response) => {
+      passport.authenticate('github', { failureRedirect: '/login' })
+      response.send('loaded')
+    })
+    .get('/logout', authenticationRequired, (request) => {
+      request.logout({}, (err) => {
+        if (err) {
+          console.log(err)
+        }
+      })
+    })
+    .get('/', async (...[, response]) => {
+      response.sendFile(resolve(paths.dist, 'index.html'))
+    })
+    .listen(port, () => {
+      console.log(`server running on port ${port}`)
+    })
+}
