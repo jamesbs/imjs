c0c9ce1dbbbe74a508a87b887adfcae6a05fae03
jamesbs <osdev@jamesbs.com>
2024-02-03 17:09:13 -0500

    handle BE failures on account check

--- app/web/Main.tsx
+++ app/web/Main.tsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from 'react'
-import { isEmptyAccount } from '../model/account'
+import { EMPTY_ACCOUNT, isEmptyAccount } from '../model/account'
+import { Account } from '@prisma/client'
 
 export const Main = () => {
-  const [account, setAccount] = useState({})
+  const [account, setAccount] = useState(EMPTY_ACCOUNT)
   const isLoggedIn = !isEmptyAccount(account)
 
   useEffect(() => {
     fetch(`/account`)
-      .then((res) => res.json())
+      .then((res) => res.json() as Account)
+      .catch((err) => {
+        setAccount(EMPTY_ACCOUNT)
+      })
       .then((res) => {
         if (!isEmptyAccount(res)) {
           setAccount(res)
