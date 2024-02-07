4e16c58d60651a925e80726595ddcda838bb9ffb
jamesbs <osdev@jamesbs.com>
2024-02-07 17:21:48 -0500

    fix reliance on deprecated api

--- app/web/index.tsx
+++ app/web/index.tsx
@@ -1,8 +1,8 @@
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { App } from './App'
 
 const root = document.querySelector('#root')
 
 if (root) {
-  render(<App />, root)
+  createRoot(root).render(<App />)
 }
